be4022522afb00e862bc51de0fa6b0014bba0dc0
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-07-11 00:08:46 -0300

    update

---
+++ README.md
@@ -0,0 +1,49 @@
+# Calculadora
+
+![Badge](http://img.shields.io/static/v1?label=STATUS&message=CONCLUIDO&color=GREEN&style=for-the-badge)
+
+<img src="https://github.com/luizfelipe9627/calculadora/blob/main/src/assets/img/apresentacao.gif" alt="Apresentação da Calculadora">
+
+## 📄 Descrição
+
+Uma calculadora é um dispositivo que permite a resolução de cálculos matemáticos. Pode funcionar através de um mecanismo eletrônico ou mecânico e ser uma máquina física ou uma ferramenta virtual.
+
+## 📑 Observação
+
+Usei esse projeto para praticar HTML5, CSS3 e principalmente JavaScript. Utilizei o Node.JS para que o Webpack e ESlint funcionasse e conseguissem ser usados para melhor desempenho do código JS.
+
+### <a href="https://luizfelipe9627-calculadora.netlify.app">Acesse o site</a>
+
+## 🎨 Layout
+
+### Créditos do layout para o <a href="https://www.figma.com/community/file/1233536186431750478">Adrei Nascimento</a>.
+
+## 💻 Tecnologias utilizadas
+
+- `HTML5`
+- `CSS3`  
+- `JAVASCRIPT`
+- `ESLINT`
+- `WEBPACK`
+- `NODE.JS`
+
+## 🧑‍💻 Criador
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/luizfelipe9627">
+        <img src="https://github.com/luizfelipe9627.png" width="100px;" alt="Foto de Luiz Felipe Silva no GitHub"/><br>
+        <sub>
+          <b>Luiz Felipe Silva</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
+
+## 📷 Me siga nas redes sociais<br>
+
+<p align="left">
+  <a href="https://www.linkedin.com/in/luizfelipe9627/" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white"></a>
+</p>
--- src/scripts/modules/calculation.js
+++ src/scripts/modules/calculation.js
@@ -1,62 +1,64 @@
-// Função responsável por fazer o cáculo da operação matemática.
+// Função Calculation responsável por fazer o cáculo da operação matemática.
 export default class Calculation {
   // O construtor é responsável por selecionar os elementos necessários para o módulo.
   constructor() {
     this.displayOperation = document.querySelector(".display-result-text"); // Está atribuindo ao this.displayOperation o elemento com a classe .display-result-text.
     this.displayResult = document.querySelector(".display-operation-text"); // Está atribuindo ao this.displayResult o elemento com a classe .display-operation-text.
 
-    // Está atribuindo ao this.numbers um array com os números de 0 a 9.
-    this.numbers = [
-      document.querySelector("#number-one"),
-      document.querySelector("#number-two"),
-      document.querySelector("#number-three"),
-      document.querySelector("#number-four"),
-      document.querySelector("#number-five"),
-      document.querySelector("#number-six"),
-      document.querySelector("#number-seven"),
-      document.querySelector("#number-eight"),
-      document.querySelector("#number-nine"),
-      document.querySelector("#number-zero"),
-      document.querySelector("#dot")
-    ];
+    this.numbers = Array.from(document.querySelectorAll(".number, .dot")); // Está atribuindo ao this.numbers um array com os números e o ponto.
+    this.operators = Array.from(document.querySelectorAll(".operator")); // Está atribuindo ao this.operators um array com os operadores matemáticos.
 
-    // Está atribuindo ao this.arithmeticOperators um array com os operadores matemáticos.
-    this.arithmeticOperators = [
-      document.querySelector("#addition"),
-      document.querySelector("#subtraction"),
-      document.querySelector("#multiplication"),
-      document.querySelector("#division"),
-    ];
-    this.acButton = document.querySelector("#ac"); // Está atribuindo ao this.acButton o elemento com o id #ac.
-    this.arrowButton = document.querySelector("#arrow"); // Está atribuindo ao this.arrowButton o elemento com o id #arrow.
-    this.equalButton = document.querySelector("#equal"); // Está atribuindo ao this.equalButton o elemento com o id #equal.
+    this.acButton = document.querySelector(".ac"); // Está atribuindo ao this.acButton o elemento com o id #ac.
+    this.arrowButton = document.querySelector(".arrow"); // Está atribuindo ao this.arrowButton o elemento com o id #arrow.
+    this.equalButton = document.querySelector(".equal"); // Está atribuindo ao this.equalButton o elemento com o id #equal.
   }
 
-  // O método addNumber é responsável por adicionar os números no display da calculadora.
-  addNumber() {
-    // Está percorrendo o array this.numbers e adicionando um evento de click em cada elemento do array.
+  // O método addEvents é responsável por adicionar os eventos de click nos elementos necessários.
+  addEvents() {
+    // O forEach é responsável por percorrer o array this.numbers e executar o código dentro dele para cada elemento do array.
     this.numbers.forEach((number) => {
-      // Está adicionando um evento de click em cada elemento do array.
-      number.addEventListener("click", () => {
-        // Está verificando se o texto do displayOperation está com o valor 0, se estiver executa o código dentro do if.
-        if (this.displayOperation.textContent === "0") {
-          this.displayOperation.textContent = ""; // Está atribuindo ao texto do this.displayOperation o valor vazio.
-        }
-
-        this.displayOperation.textContent += number.value; // Está atribuindo ao texto do this.displayOperation o valor do número clicado.
-      });
+      // Está adicionando um evento de click em cada elemento do array this.numbers.
+      number.addEventListener(
+        "click",
+        () => this.handleNumberClick(number.value) // Está chamando o método handleNumberClick responsável por adicionar os números no display da calculadora e passando como parâmetro o valor do número clicado.
+      );
     });
+
+    // O forEach é responsável por percorrer o array this.operators e executar o código dentro dele para cada elemento do array.
+    this.operators.forEach((operator) => {
+      // Está adicionando um evento de click em cada elemento do array this.operators.
+      operator.addEventListener(
+        "click",
+        () => this.handleOperatorClick(operator) // Está chamando o método handleOperatorClick responsável por adicionar os operadores matemáticos no display da calculadora e passando como parâmetro o valor do operador matemático clicado.
+      );
+    });
+
+    // Está adicionando um evento de click no botão de igual e chamando o método calculateResult responsável por calcular o resultado da operação matemática.
+    this.equalButton.addEventListener("click", () => this.calculateResult());
+
+    // Está adicionando um evento de click no botão de AC e chamando o método clearDisplay responsável por limpar o display da calculadora.
+    this.acButton.addEventListener("click", () => this.clearDisplay());
+
+    // Está adicionando um evento de click no botão de seta e chamando o método removeLastCharacter responsável por remover o último caractere do display da calculadora.
+    this.arrowButton.addEventListener(
+      "click",
+      () => this.removeLastCharacter() // Está chamando o método removeLastCharacter responsável por remover o último caractere do display da calculadora.
+    );
   }
 
-  // O método operationMath é responsável por adicionar os operadores matemáticos no display da calculadora.
-  operationMath() {
-    // Está percorrendo o array this.arithmeticOperators e adicionando um evento de click em cada elemento do array.
-    this.arithmeticOperators.forEach((operator) => {
-      // Está adicionando um evento de click em cada elemento do array.
-      operator.addEventListener("click", () => {
-        this.displayOperation.textContent += ` ${operator.value} `; // Está atribuindo ao texto do this.displayOperation o valor do operador matemático clicado.
-      });
-    });
+  // O método handleNumberClick é responsável por adicionar os números no display da calculadora.
+  handleNumberClick(number) {
+    // Está verificando se o texto do displayOperation está com o valor 0, se estiver executa o código dentro do if.
+    if (this.displayOperation.textContent === "0") {
+      this.displayOperation.textContent = ""; // Está atribuindo ao texto do this.displayOperation o valor vazio.
+    }
+
+    this.displayOperation.textContent += number; // Está atribuindo ao texto do this.displayOperation o valor do número clicado.
+  }
+
+  // O método handleOperatorClick é responsável por adicionar os operadores matemáticos no display da calculadora.
+  handleOperatorClick(operator) {
+    this.displayOperation.textContent += ` ${operator.value} `; // Está atribuindo ao texto do this.displayOperation o valor do operador matemático clicado.
   }
 
   // O método calculateResult é responsável por calcular o resultado da operação matemática.
@@ -64,12 +66,13 @@ export default class Calculation {
     // O try catch é responsável por tentar executar o código dentro do try, se der erro executa o código dentro do catch.
     try {
       const expression = this.displayOperation.textContent; // Está atribuindo a const expression o valor do texto do this.displayOperation.
-      const result = eval(expression); // O eval é responsável por calcular a expressão matemática do texto do this.displayOperation e atribuir o resultado a const result.
 
-      const formattedResult = result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Está atribuindo a const formattedResult ao resultado, utilizando o método toString para transformar o resultado em string e o método replace para substituir os pontos por vírgulas utilizando uma expressão regular.
+      const result = eval(expression); // Está atribuindo a const result o resultado da expressão matemática utilizando o método eval que faz o cálculo da expressão.
+
+      const formattedResult = this.formatResult(result, expression); // Está atribuindo a const formattedResult o resultado do método formatResult que formata o resultado da expressão matemática e passando como parâmetro o resultado da expressão e o operador matemático utilizado.
 
       this.displayResult.textContent = formattedResult; // Está atribuindo ao texto do this.displayResult o valor da const result.
-    } catch (error) {      
+    } catch (error) {
       this.displayResult.textContent = "Operação inválida"; // Está atribuindo ao texto do this.displayResult o valor "Operação inválida".
       this.displayResult.style.fontSize = "28px"; // Está atribuindo ao estilo do this.displayResult o valor 28px.
       this.displayResult.style.color = "red"; // Está atribuindo ao estilo do this.displayResult a cor vermelha.
@@ -79,46 +82,72 @@ export default class Calculation {
         this.displayResult.textContent = "0"; // Está atribuindo ao texto do this.displayResult o valor 0.
         this.displayResult.style.fontSize = "48px"; // Está atribuindo ao estilo do this.displayResult o valor 48px.
         this.displayResult.style.color = ""; // Está atribuindo ao estilo do this.displayResult a cor padrão definida no CSS.
-      }, 1500);
+      }, 2000);
     }
+  }
+
+  formatResult(result, operation) {
+    let formattedResult; // Está criando a variável formattedResult.
+
+    // Está verificando qual operador matemático foi utilizado e formatando o resultado de acordo com o operador.
+    switch (operation) {
+      case "+":
+        formattedResult = result.toLocaleString(); // Formata o resultado com as configurações regionais padrão.
+        break;
+      case "-":
+        formattedResult = result.toFixed(2); // Formata o resultado com 2 casas decimais.
+        break;
+      case "*":
+        formattedResult = result.toExponential(2); // Formata o resultado em notação exponencial com 2 casas decimais.
+        break;
+      case "/":
+        formattedResult = result.toPrecision(4); // Formata o resultado com até 4 dígitos significativos.
+        break;
+      default:
+        formattedResult = result.toString(); // Formata o resultado como uma string padrão.
+        break;
+    }
+
+    return formattedResult;
   }
 
   // O método resultMath é responsável por chamar o método calculateResult quando o botão de igual for clicado.
   resultMath() {
     // Está adicionando um evento de click no botão de igual.
     this.equalButton.addEventListener("click", () => {
-      this.calculateResult(); // Está chamando o método calculateResult.
+      this.calculateResult(); // Está chamando o método calculateResult responsável por calcular o resultado da operação matemática.
     });
   }
 
   // O método cleanDisplay é responsável por limpar o display da calculadora.
-  cleanDisplay() {
-    // Está adicionando um evento de click no botão de AC.
-    this.acButton.addEventListener("click", () => {
-      // Está atribuindo ao texto do this.displayOperation e this.displayResult o valor 0.
-      this.displayOperation.textContent = "0";
-      this.displayResult.textContent = "0";
-    });
+  clearDisplay() {
+    // Está atribuindo ao texto do this.displayOperation e this.displayResult o valor 0.
+    this.displayOperation.textContent = "0";
+    this.displayResult.textContent = "0";
+  }
 
+  // O método removeLastCharacter é responsável por remover o último caractere do display da calculadora.
+  removeLastCharacter() {
     // Está adicionando um evento de click no botão de seta.
-    this.arrowButton.addEventListener("click", () => {
-      this.displayOperation.textContent = this.displayOperation.textContent.slice(0, -1).trim(); // Está atribuindo ao texto do displayOperation o valor do texto do displayOperation sem o último caractere(com o slice) e removendo os espaços em branco(com o trim).
+    this.displayOperation.textContent = this.displayOperation.textContent.slice(
+      0,
+      -1
+    ); // Está atribuindo ao texto do displayOperation o valor do texto do displayOperation sem o último caractere(com o slice) e removendo os espaços em branco(com o trim).
 
-      // Está verificando se o texto do displayOperation está vazio, se estiver executa o código dentro do if.
-      if (this.displayOperation.textContent === "") {
-        this.displayOperation.textContent = "0"; // Está atribuindo ao texto do this.displayOperation o valor 0.
-      }
-    });
+    // Está verificando se o texto do displayOperation está vazio, se estiver executa o código dentro do if.
+    if (this.displayOperation.textContent === "") {
+      this.displayOperation.textContent = "0"; // Está atribuindo ao texto do this.displayOperation o valor 0.
+    }
   }
 
   // O método init é responsável por chamar os métodos que serão usados na classe.
   init() {
-    if(this.displayOperation && this.displayResult) {
-      // Está chamando os métodos.
-      this.addNumber();
-      this.operationMath();
-      this.resultMath();
-      this.cleanDisplay();
+    // Está verificando se o this.displayOperation e this.displayResult existem, se sim executa o if.
+    if (this.displayOperation && this.displayResult) {
+      // Está chamando o método addEvents.
+      this.addEvents();
     }
+
+    return this; // Está retornando o this para que seja possível acessar os métodos do módulo através do init.
   }
 }
